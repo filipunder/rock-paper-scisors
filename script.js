f6e97efde32e74f2017c61c35f84a10631beb954
Filipunder <filipov8205@gmail.com>
2024-04-22 05:15:55 -0700

    Add human player choice function

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3) + 1;
   if (choice == 1) {
-    choice = "Rock";
+    choice = "rock";
   } else if (choice == 2) {
-    choice = "Paper";
+    choice = "paper";
   } else {
-    choice = "Scisors";
+    choice = "scissors";
   }
   return choice;
 }
+
+function getHumanChoice() {
+  let hChoice = prompt("Make your choice:");
+  let text;
+  if (hChoice == null || hChoice == "") {
+    return (text = "Invalid Input");
+  } else if (hChoice == "rock" || hChoice == "paper" || hChoice == "scissors") {
+    return hChoice;
+  } else {
+    return (text = "Please make a valid choice");
+  }
+}
+
+// function playRound(playerSelection, computerSelection) {
+//   if (playerSelection == computerSelection) {
+//
+//   }
+//}
